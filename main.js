611ffbf665ae181462b564797287ff03c4ee07a7
Igor <defleppard333@gmail.com>
2021-04-27 15:51:52 +0300

    task 1

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+let numberOfFilms = +prompt("How many films have you seen?");
+let personalMoiveDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+};
+
+const a = prompt("One of the last film have you watched", ""),
+   b = prompt("what a rating of this film");
+
+
+console.log(personalMoiveDB.movies.a = b);
+
+
+
+
+
