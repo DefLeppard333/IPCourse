fdc2b4d6bfdae7aa9b02e9657a4434f870281bb7
Igor <defleppard333@gmail.com>
2021-04-29 15:21:03 +0300

    task 2

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 'use strict';
 let numberOfFilms = +prompt("How many films have you seen?");
-let personalMoiveDB = {
+
+
+let personalMovieDB = {
    count: numberOfFilms,
    movies: {},
    actors: {},
    genres: [],
    privat: false,
 };
+for (let i = 0; i < 2; i++) {
 
-const a = prompt("One of the last film have you watched", ""),
-   b = prompt("what a rating of this film");
-personalMoiveDB.movies[a] = b;
+   if (personalMovieDB.count < 10) {
+      console.log('not enough films you have watched');
+      break;
+   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log('you are cool');
+   } else if (personalMovieDB.count >= 30) {
+      console.log(' you love films');
+   } else {
+      console.log('error');
+   }
 
-console.log(personalMoiveDB);
+   let a = prompt("what a two last films have you watched", ""),
+      b = prompt("what a rating of this film");
+
+
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+   } else {
+      console.log('error');
+      i--;
+   }
+}
+
+console.log(personalMovieDB);
 
 
 
